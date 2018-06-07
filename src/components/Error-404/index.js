ef9a0c258955f91a4e447d63f88692a03c583406
ErykSandecki <eryk787@wp.pl>
2018-06-07 23:12:37 +0200

    Create 404 logo

--- src/components/Error-404/index.js
+++ src/components/Error-404/index.js
@@ -1,14 +1,21 @@
-import React, { Component } from 'react';
-
 import './style.css';
+
+import React, { Component } from 'react';
 
 export default class Error404 extends Component {
     render() {
         return  <div className="error">
-                    <img className="error-tube-logo-image"
-                         src={require('../../Images/Error-404.png')}
-                         alt="error-tube-logo"
-                    />
+                    <div className="error-title">
+                        <div className="error-text">
+                            <span className="error-letter" data-letter="4">4</span>
+                            <span className="error-letter" data-letter="0">0</span>
+                            <span className="error-letter" data-letter="4">4</span>
+                        </div>
+                        <img className="error-tube-logo-image"
+                             src={require('../../Images/Error-404.png')}
+                            alt="error-tube-logo"
+                        />
+                    </div>
                 </div>
     }
 }
