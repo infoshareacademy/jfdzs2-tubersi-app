a87a3a38e33212e0c09a102b7add9e988b137859
ErykSandecki <eryk787@wp.pl>
2018-06-11 22:16:05 +0200

    s

--- src/components/Error-404/index.js
+++ src/components/Error-404/index.js
@@ -1,21 +1,26 @@
-import './style.css';
-
 import React, { Component } from 'react';
 
+import './style.css';
+
 export default class Error404 extends Component {
+
     render() {
         return  <div className="error">
                     <div className="error-title">
-                        <div className="error-text">
-                            <span className="error-letter" data-letter="4">4</span>
-                            <span className="error-letter" data-letter="0">0</span>
-                            <span className="error-letter" data-letter="4">4</span>
-                        </div>
-                        <img className="error-tube-logo-image"
-                             src={require('../../Images/Error-404.png')}
-                            alt="error-tube-logo"
+                        <img className="error-message"
+                             src={require('../../Images/Error-message.png')}
+                             alt="error-message"
                         />
+                    </div>
+                    <div className="error-content col-xs-10 col-xs-offset-1">
+                        <p className="error-help-text">Znajdź szukaną podstronę, kawałku, lub playlistę na Tubersi</p>
+                        <label>Name</label>
+                        <input type="text" name="name"/>
+                        <span>Enter your full name here</span>
                     </div>
                 </div>
     }
 }
+
+
+ 
