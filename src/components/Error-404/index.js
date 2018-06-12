c4290fde84f6634805c6ec7021b12f888fdf922a
ErykSandecki <eryk787@wp.pl>
2018-06-12 19:22:29 +0200

    Complete Design error-404

--- src/components/Error-404/index.js
+++ src/components/Error-404/index.js
@@ -1,45 +1,52 @@
-import React, { Component } from 'react';
-
 import './style.css';
+
+import React, { Component } from 'react';
 
 export default class Error404 extends Component {
 
     render() {
         return  <div className="error">
                     <div className="error-title">
-                        <img className="error-message"
-                             src={require('../../Images/Error-message.png')}
-                             alt="error-message"
-                        />
+                        <div className="error-title-contain">
+                            <p className="error-title-contain-text-404">
+                                <span className="error-title-contain-text-404-warning glyphicon glyphicon-warning-sign"></span>
+                                ERROR 404
+                            </p>
+                            <p className="error-title-contain-text-alert">
+                                Strona której aktualnie szukasz, nie istnieje lub nie jest dostępna.
+                            </p>
+                        </div>
                     </div>
                     <div className="error-content col-xs-10 col-xs-offset-1 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3 col-lg-4 col-lg-offset-4">
-                        <p className="error-help-text">
+                        <p className="error-content-help-text">
                             Znajdź szukaną podstronę, kawałku, lub playlistę na Tubersi:
                         </p>
-                        <input className="error-input-search col-xs-12 col-sm-8"
+                        <input className="error-content-input-search col-xs-12 col-sm-8"
                                placeholder="Szukaj..."
                         />
-                        <button className="error-button-search col-xs-12 col-sm-2">Szukaj</button>
+                        <button className="error-content-button-search col-xs-12 col-sm-2">Szukaj</button>
                     </div>
-                    <div className="error-help col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
-                        <p className="error-suggestion col-xs-3">
+                    <div className="error-content-help col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
+                        <p className="error-content-help-suggestion col-xs-12 col-sm-2">
                             Kontakt
                         </p>
-                        <span className="error-suggestion-space col-xs-1">
-                            --
+                        <span className="error-content-help-suggestion-space col-xs-12 col-sm-3">
+                            ---------------
                         </span>
-                        <p className="error-suggestion col-xs-3">
-                            Tubersi Status
+                        <p className="error-content-help-suggestion col-xs-12 col-sm-2">
+                            Status
                         </p>
-                        <span className="error-suggestion-space col-xs-1">
-                            --
+                        <span className="error-content-help-suggestion-space col-xs-12 col-sm-3">
+                            ---------------
                         </span>
-                        <p className="error-suggestion col-xs-3">
+                        <p className="error-content-help-suggestion col-xs-12 col-sm-2">
                             @tubersi
                         </p>
                     </div>
                     <div className="error-icon">
-                        <img src={require('../../Images/Youtube-icon-64-64.png')}/>
+                        <img src={require('../../Images/Youtube-icon-64-64.png')}
+                             alt="error-icon"
+                        />
                     </div>
                 </div>
     }
