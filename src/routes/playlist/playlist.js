c56171a3fe0cb84309515013a3a01dc21c289d75
PiotrMajkowski <piotr.majkowski@gmail.com>
2018-06-23 23:52:43 +0200

    add playlist view

--- src/routes/playlist/playlist.js
+++ src/routes/playlist/playlist.js
@@ -2,16 +2,53 @@ import React, { PureComponent } from 'react';
 import './playlist.css';
 
 
-
 class Playlist extends PureComponent {
   render() {
     return (
       <div className="content container-fluid">
         <div className="row">
-          <div className="col-xs-12">
+          <div className="col-xs-12" className="playlist">
             <h1>Playlista</h1>
-            <p>lorem ibsum ble bl ble ble</p>
-          </div>
+
+            <div className="playlist-item">
+              <div class="embed-responsive embed-responsive-16by9">
+                  <iframe 
+                    class="embed-responsive-item" 
+                    src="https://www.youtube.com/embed/Ur_tXqaNXOI"></iframe>
+              </div>
+              <button 
+                type="button" 
+                className="btn btn-default btn-lg btn-playlist-item">
+                <i class="fas fa-trash-alt"></i>Usuń z playlisty
+              </button>
+           </div>
+            
+            <div className="playlist-item">
+              <div class="embed-responsive embed-responsive-16by9">
+                  <iframe 
+                  class="embed-responsive-item" 
+                  src="https://www.youtube.com/embed/Ur_tXqaNXOI"></iframe>
+              </div>
+              <button 
+                type="button" 
+                className="btn btn-default btn-lg btn-playlist-item">
+                <i class="fas fa-trash-alt"></i>Usuń z playlisty
+              </button>
+            </div>
+
+         
+            <div className="playlist-buttons">
+              <p>
+                <button type="button" className="btn btn-default btn-lg btn-playlist">
+                  <i class="fas fa-share-alt"></i>Udostepnij playlistę
+                </button>
+                <button type="button" className="btn btn-default btn-lg btn-playlist">
+                  <i className="fab fa-youtube"></i>Graj playliste
+                </button>
+              </p>
+            </div>        
+        </div>
+        
         </div>
       </div>
     );
