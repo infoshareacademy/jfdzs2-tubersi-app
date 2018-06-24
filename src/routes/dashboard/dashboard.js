f7ff53956f6897418526e094dac2f9f88ce89c0e
Dorota Pawłowska <dpawlowska@op.pl>
2018-06-24 12:44:35 +0200

    added bootstrap columns to charts and something else

--- src/routes/dashboard/dashboard.js
+++ src/routes/dashboard/dashboard.js
@@ -24,7 +24,7 @@ class Dashboard extends PureComponent {
     data2 = [
         {name: 'Group A', value: 400}, {name: 'Group B', value: 300},
         {name: 'Group C', value: 300}, {name: 'Group D', value: 200}
-        ];
+    ];
 
     componentDidMount(){
 
