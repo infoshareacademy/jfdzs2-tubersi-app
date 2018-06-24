12d28083d97f6a7af07a17d88f6fb5a3b4592ec4
PiotrMajkowski <piotr.majkowski@gmail.com>
2018-06-24 07:51:12 +0200

    add playlists view

--- src/routes/yourPlaylists/yourPlaylists.js
+++ src/routes/yourPlaylists/yourPlaylists.js
@@ -9,7 +9,37 @@ class YourPlaylists extends PureComponent {
         <div className="row">
           <div className="col-xs-12">
             <h1>Lista wszystkich playlist</h1>
-            <p>lorem ibsum ble bl ble ble</p>
+            <div className="playlist-item">
+              <p>Nazwa playlisty nr 1</p>
+              <p>
+                <button type="button" className="btn btn-default btn-lg btn-playlists">
+                  <i className="fab fa-youtube"></i>Graj playliste
+                </button>
+                <button type="button" className="btn btn-default btn-lg btn-playlists">
+                  <i class="fas fa-share-alt"></i>Udostepnij playlistę
+                </button>
+                <button type="button" className="btn btn-default btn-lg btn-playlists">
+                  <i className="fas fa-trash-alt"></i>Usuń playlistę
+                </button>
+              </p>
+            </div>
+
+            <div className="playlist-item">
+              <p>Nazwa playlisty nr 2</p>
+              <p>
+                <button type="button" className="btn btn-default btn-lg btn-playlists">
+                  <i className="fab fa-youtube"></i>Graj 
+                </button>
+                <button type="button" className="btn btn-default btn-lg btn-playlists">
+                  <i class="fas fa-share-alt"></i>Udostepnij
+                </button>
+                <button type="button" className="btn btn-default btn-lg btn-playlists">
+                  <i className="fas fa-trash-alt"></i>Usuń
+                </button>
+              </p>
+            </div>
+
+
           </div>
         </div>
       </div>
