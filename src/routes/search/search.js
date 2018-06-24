878080fefa0c3fab725a8477e3feabb20d34313c
PiotrMajkowski <piotr.majkowski@gmail.com>
2018-06-24 12:02:22 +0200

    simple change in search view

--- src/routes/search/search.js
+++ src/routes/search/search.js
@@ -7,18 +7,17 @@ class Search extends PureComponent {
   render() {
     return (
       <div className="content-search container-fluid">
-            <h1>Rozpocznij wyszukiwanie</h1>    
+      <div className="row">
+        <div className="col-xs-12 col-md-push-2 col-md-8" >
+          <h1>Rozpocznij wyszukiwanie</h1>
+          <form>
+            <input placeholder="Wpisz czego szukasz?" className="input-search animated bounceInRight"/>
+            <button className="btn btn-default btn-lg btn-search animated bounceInRight">Szukaj</button>
+          </form> 
         
-             
-              <input placeholder="Wpisz czego szukasz?" className="input-search animated bounceInRight"/>
-              <button className="btn btn-default btn-lg btn-search animated bounceInRight">Szukaj</button>
-
-           
-            
-        </div>
-        
-    
-
+         </div>
+      </div>
+      </div>
     );
   }
 }
