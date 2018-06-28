2877bfa863c46d45a14c5f80c01bfb29d9eed1fc
PiotrMajkowski <piotr.majkowski@gmail.com>
2018-06-28 09:28:26 +0200

    add search result view

--- src/routes/searchResults/searchResults.js
+++ src/routes/searchResults/searchResults.js
@@ -7,72 +7,132 @@ import '../playlist/playlist.css';
 class SearchResults extends PureComponent {
   render() {
     return (
-      <div className="content container-fluid">
+      <div className="content">
+
         <div className="row">
-          <div className="col-xs-12 playlist">
-            <h1>Wyniki wyszukiwania</h1>
-
-            <div className="row playlist-item animated bounceInRight">
-            <div className="col-md-6 playlist-item-video">
-              <div className=" embed-responsive embed-responsive-4by3">
-                  <iframe 
-                    class="embed-responsive-item" 
-                    src="https://www.youtube.com/embed/Ur_tXqaNXOI"></iframe>
+          <div className="col-sm-6 col-md-4">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="playlist-item animated bounceInRight">
+                  <div className="embed-responsive embed-responsive-4by3">
+                      <iframe
+                        src="https://www.youtube.com/embed/qGRt_t24jjI"></iframe>
+                  </div>
+                  <button 
+                    type="button" 
+                    className="btn btn-default btn-playlist">
+                    <i class="fas fa-trash-alt"></i>Usuń
+                  </button>
+                  <button 
+                    type="button" 
+                    className="btn btn-default btn-playlist">
+                    <i class="fas fa-trash-alt"></i>Dodaj
+                  </button>
+                </div>
               </div>
             </div>
-              <div className="col-md-6">
-                <button 
-                  type="button" 
-                  className="btn btn-default btn-lg btn-playlist-item">
-                  <i class="fas fa-plus"></i>Dodaj do nowej playlisty
-                </button>
-                <button 
-                  type="button" 
-                  className="btn btn-default btn-lg btn-playlist-item">
-                  <i class="far fa-list-alt"></i>Dodaj do playlisty nr 1
-                </button>
-                <button 
-                  type="button" 
-                  className="btn btn-default btn-lg btn-playlist-item">
-                  <i class="far fa-list-alt"></i>Dodaj do playlisty nr 2
-                </button>
-              </div>
-            
-           </div>
+          </div>
 
-
-           <div className="row playlist-item animated bounceInRight">
-            <div className="col-md-6 playlist-item-video">
-              <div className=" embed-responsive embed-responsive-4by3">
-                  <iframe 
-                    class="embed-responsive-item" 
-                    src="https://www.youtube.com/embed/Ur_tXqaNXOI"></iframe>
+          <div className="col-sm-6 col-md-4">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="playlist-item animated bounceInRight">
+                  <div className="embed-responsive embed-responsive-4by3">
+                      <iframe 
+                        src="https://www.youtube.com/embed/Ur_tXqaNXOI"></iframe>
+                  </div>
+                  <button 
+                    type="button" 
+                    className="btn btn-default btn-playlist">
+                    <i class="fas fa-trash-alt"></i>Usuń
+                  </button>
+                </div>
               </div>
             </div>
-              <div className="col-md-6">
-                <button 
-                  type="button" 
-                  className="btn btn-default btn-lg btn-playlist-item">
-                  <i class="fas fa-plus"></i>Dodaj do nowej playlisty
-                </button>
-                <button 
-                  type="button" 
-                  className="btn btn-default btn-lg btn-playlist-item">
-                  <i class="far fa-list-alt"></i>Dodaj do playlisty nr 1
-                </button>
-                <button 
-                  type="button" 
-                  className="btn btn-default btn-lg btn-playlist-item">
-                  <i class="far fa-list-alt"></i>Dodaj do playlisty nr 2
-                </button>
+          </div>
+
+          <div className="col-sm-6 col-md-4">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="playlist-item animated bounceInRight">
+                  <div className="embed-responsive embed-responsive-4by3">
+                      <iframe 
+                        src="https://www.youtube.com/embed/CsUS-jRB6Cc"></iframe>
+                  </div>
+                  <button 
+                    type="button" 
+                    className="btn btn-default btn-playlist">
+                    <i class="fas fa-trash-alt"></i>Usuń
+                  </button>
+                </div>
               </div>
-            
-           </div>
-            
-        </div>
+            </div>
+          </div>
+
+          <div className="col-sm-6 col-md-4">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="playlist-item animated bounceInRight">
+                  <div className="embed-responsive embed-responsive-4by3">
+                      <iframe 
+                        src="https://www.youtube.com/embed/qGRt_t24jjI"></iframe>
+                  </div>
+                  <button 
+                    type="button" 
+                    className="btn btn-default btn-playlist">
+                    <i class="fas fa-trash-alt"></i>Usuń
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm-6 col-md-4">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="playlist-item animated bounceInRight">
+                  <div className="embed-responsive embed-responsive-4by3">
+                      <iframe 
+                        src="https://www.youtube.com/embed/Ur_tXqaNXOI"></iframe>
+                  </div>
+                  <button 
+                    type="button" 
+                    className="btn btn-default btn-playlist">
+                    <i class="fas fa-trash-alt"></i>Usuń
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="col-sm-6 col-md-4">
+            <div className="row">
+              <div className="col-md-12">
+                <div className="playlist-item animated bounceInRight">
+                  <div className="embed-responsive embed-responsive-4by3">
+                      <iframe 
+                        src="https://www.youtube.com/embed/CsUS-jRB6Cc"></iframe>
+                  </div>
+                  <button 
+                    type="button" 
+                    className="btn btn-default btn-playlist">
+                    <i class="fas fa-trash-alt"></i>Usuń
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          
         
-        </div>
+         
+
+      </div>   
+
       </div>
+
+
+
     );
   }
 }
