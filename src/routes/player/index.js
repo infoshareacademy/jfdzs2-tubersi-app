bc50a8fe4b659b939e88795c9a8100b7dd707a77
ErykSandecki <eryk787@wp.pl>
2018-07-07 13:02:09 +0200

    Create logic mechanic autoplay playlist and set next playlist

--- src/routes/player/index.js
+++ src/routes/player/index.js
@@ -1,31 +1,58 @@
 import React, { PureComponent} from 'react';
+import YouTube from 'react-youtube';
 
 import Layout from '../../components/layout';
-import ReactPlayer from 'react-player'
+
 
 import './style.css';
 
 class Player extends PureComponent {
+  
+  constructor(props) {
+    super(props);
+    this.state = {
+      id: "MzuTuSRobaE",
+    }
+  }
+  _onReady(event) {
+    // access to player in all event handlers via event.target
+    event.target.playVideo();
+  }
+
+  func = () => {
+    this.setState({
+      id: "HMU5U06UwOs"
+    })
+  }
+
   render() {
+    const opts = {
+      height: '390',
+      width: '640',
+      playerVars: { 
+        autoplay: 1
+      }
+    };
     return (
       <Layout>
       <div className="content container-fluid">
         <div className="row">
           <div className="col-xs-12">
             <div className="player-wrapper">
-            <ReactPlayer url='https://www.youtube.com/watch?v=ysz5S6PUM-U'
-                         className='react-player'
-                         playing={false}
-                         width='100%'
-                         height='100%'
-                         controls
-            />
+              <YouTube
+                videoId={this.state.id}
+                opts={opts}
+                onReady={this._onReady}
+                onEnd={this.func}
+              />
             </div>
           </div>
         </div>
       </div>
       </Layout>
     );
+
+    
   }
 }
 
