ad6fffd87c9eba42d281c1760c2f3012ebb4e695
PiotrMajkowski <piotr.majkowski@gmail.com>
2018-07-09 21:24:52 +0200

    transfer sign-out btn for mobile from header to mobile menu

--- src/components/menu/index.js
+++ src/components/menu/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import './style.css'
 
-export default class Menu extends Component {
+class Menu extends Component {
     render() {
         var visibility = "menu";
         if (this.props.menuVisibility) {
@@ -16,10 +17,25 @@ export default class Menu extends Component {
                 <ul>
                     <Link className="menu-link" to="/"><li><i className="fas fa-chart-line"></i><span>Dashboard</span></li></Link>
                     <Link className="menu-link" to="/search"><li><i className="fas fa-search"></i><span>Szukaj</span></li></Link>
-                    <Link className="menu-link" to="/your-playlist"><li><i className="fas fa-list"></i><span>Twoje playlisty</span></li></Link>
+                    <Link className="menu-link" to="/your-playlist"><li><i className="fas fa-list"></i><span>Playlisty</span></li></Link>
                     <Link className="menu-link" to="/chat"><li><i className="fas fa-comments"></i><span>Chat</span></li></Link>
+                    <div className="menu-link menu-link-sign-out" onClick={this.props.changeStatus}><li><i className="fas fa-sign-out-alt"></i><span>Wyloguj</span></li></div>
                 </ul> 
             </div>
         );
     }
 }
+
+const mapStateToProps = status => {
+    return {
+        status,
+    };
+  };
+  
+const mapDispatchToProps = dispatch => {
+    return {
+        changeStatus: () => dispatch({ type: 'OFFLINE'}),
+    };
+  };
+  
+export default connect (mapStateToProps, mapDispatchToProps)(Menu);
