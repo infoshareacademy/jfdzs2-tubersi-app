c28642baaef20323bfbdc069fd9a1fe4934bfa1f
Dorota Pawłowska <dpawlowska@op.pl>
2018-07-11 21:18:17 +0200

    work with charts in progress - everything looks almost good

--- src/routes/dashboard/index.js
+++ src/routes/dashboard/index.js
@@ -29,13 +29,13 @@ const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
 class Dashboard extends PureComponent {
 
     data = [
-        {name: 'Page A', uv: 4000, pv: 2400, amt: 2400},
-        {name: 'Page B', uv: 3000, pv: 1398, amt: 2210},
-        {name: 'Page C', uv: 2000, pv: 9800, amt: 2290},
-        {name: 'Page D', uv: 2780, pv: 3908, amt: 2000},
-        {name: 'Page E', uv: 1890, pv: 4800, amt: 2181},
-        {name: 'Page F', uv: 2390, pv: 3800, amt: 2500},
-        {name: 'Page G', uv: 3490, pv: 4300, amt: 2100},
+        {name: 'Page A', zielony: 4000, fiolet: 2400, amt: 2400},
+        {name: 'Page B', zielony: 3000, fiolet: 1398, amt: 2210},
+        {name: 'Page C', zielony: 2000, fiolet: 9800, amt: 2290},
+        {name: 'Page D', zielony: 2780, fiolet: 3908, amt: 2000},
+        {name: 'Page E', zielony: 1890, fiolet: 4800, amt: 2181},
+        {name: 'Page F', zielony: 2390, fiolet: 3800, amt: 2500},
+        {name: 'Page G', zielony: 3490, fiolet: 4300, amt: 2100},
     ];
 
     data2 = [
@@ -47,20 +47,20 @@ class Dashboard extends PureComponent {
     return (
         <Layout>
             <UserAuthorized />
-            <div className="content container">
-                <div className="row">
-                    <div className="col-xs-12">
-                        <div className="chartbackground col-xs-12">
+            <div className="container ">
+                <div className="row main-margin">
+                        <div className="displey-content col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                            <div className="chart-background">
                                 <LineChart
-                                    width={500}
-                                    height={300}
+                                    width={450}
+                                    height={250}
                                     data={this.data}
                                     margin={{
-                                        top: 5,
-                                        right: 30,
+                                        top: 20,
+                                        right:  20,
                                         left: 20,
-                                        bottom: 5
-                                            }}
+                                        bottom: 20
+                                    }}
                                 >
                                     <XAxis dataKey="name"/>
                                     <YAxis/>
@@ -69,28 +69,30 @@ class Dashboard extends PureComponent {
                                     <Legend />
                                     <Line
                                         type="monotone"
-                                        dataKey="pv"
+                                        dataKey="fiolet"
                                         stroke="#8884d8"
                                         activeDot={{r: 8}}
                                     />
                                     <Line
                                         type="monotone"
-                                        dataKey="uv"
+                                        dataKey="zielony"
                                         stroke="#82ca9d"
                                     />
                                 </LineChart>
+                            </div>
                         </div>
-                        <div className="chartbackground col-xs-12 ">
+                    <div className="displey-content col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <div className="chart-background">
                             <BarChart
-                                width={500} 
-                                height={300} 
+                                width={450}
+                                height={250}
                                 data={this.data}
                                 margin={{
-                                    top: 5, 
-                                    right: 30, 
-                                    left: 20, 
-                                    bottom: 5
-                                        }}
+                                    top: 20,
+                                    right:  20,
+                                    left: 20,
+                                    bottom: 20
+                                }}
                             >
                                 <CartesianGrid strokeDasharray="3 3"/>
                                 <XAxis dataKey="name"/>
@@ -98,19 +100,21 @@ class Dashboard extends PureComponent {
                                 <Tooltip/>
                                 <Legend />
                                 <Bar 
-                                    dataKey="pv" 
+                                    dataKey="fiolet"
                                     fill="#8884d8" 
                                 />
                                 <Bar 
-                                    dataKey="uv" 
+                                    dataKey="zielony"
                                     fill="#82ca9d" 
                                 />
                             </BarChart>
                         </div>
-                        <div className="chartbackground col-xs-12 ">
+                        </div>
+                    <div className="displey-content col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <div className="chart-background">
                             <PieChart
-                                width={500} 
-                                height={300} 
+                                width={450}
+                                height={250}
                                 onMouseEnter={this.onPieEnter} 
                             >
                                 <Pie
@@ -125,17 +129,19 @@ class Dashboard extends PureComponent {
                                 </Pie>
                             </PieChart>
                         </div>
-                        <div className="chartbackground col-xs-12">
+                        </div>
+                    <div className="displey-content col-xs-12 col-sm-12 col-md-6 col-lg-6">
+                        <div className="chart-background">
                             <AreaChart
-                                width={500} 
-                                height={300} 
+                                width={450}
+                                height={250}
                                 data={this.data}
                                 margin={{
-                                    top: 10, 
-                                    right: 30, 
-                                    left: 0, 
-                                    bottom: 0
-                                        }}
+                                    top: 20,
+                                    right:  20,
+                                    left: 20,
+                                    bottom: 20
+                                }}
                             >
                                 <CartesianGrid strokeDasharray="3 3"/>
                                 <XAxis dataKey="name"/>
@@ -143,14 +149,14 @@ class Dashboard extends PureComponent {
                                 <Tooltip/>
                                 <Area 
                                     type='monotone' 
-                                    dataKey='uv' 
+                                    dataKey='zielony'
                                     stroke='#8884d8' 
                                     fill='#8884d8' 
                                 />
                             </AreaChart>
                         </div>
+                        </div>
                     </div>
-                </div>
             </div>
         </Layout>
     );
